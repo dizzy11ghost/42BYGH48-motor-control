cdbef3185e141ee6a4fb63b10bce48c429947e4a
Lego Leñero <82170264+dizzy11ghost@users.noreply.github.com>
2026-06-01 17:17:22 -0600

    Refactor ESP32 firmware for better readability

--- firmware_esp32.ino
+++ firmware_esp32.ino
@@ -5,18 +5,22 @@ const int STEP_PIN = 12;
 const int DIR_PIN = 14;
 const int ENABLE_PIN = 13;
 
+const float VELOCIDAD_MAX = 2000.0;
+const float ACELERACION = 800.0;
+const long VELOCIDAD_SERIAL = 115200;
+
 // Inicialización del motor en modo Driver
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(VELOCIDAD_SERIAL);
 
     pinMode(ENABLE_PIN, OUTPUT);
     digitalWrite(ENABLE_PIN, LOW); // Activar el driver
 
     // Configuración inicial de límites de seguridad
-    stepper.setMaxSpeed(2000); // Pasos por segundo máximo
-    stepper.setAcceleration(800); // Aceleración (Control de rampa)
+    stepper.setMaxSpeed(VELOCIDAD_MAX); // Pasos por segundo máximo
+    stepper.setAcceleration(ACELERACION); // Aceleración (Control de rampa)
 }
 
 void loop() {
